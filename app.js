9fc5af73a32b0f6510ce13352d0c7d87b67b5ac5
Vincent Nadoll <vincent.nadoll@gmail.com>
2018-02-22 22:20:17 +0100

    Add express server
    - Add logger suite
    - Add mongoose
    - Configure general API routes

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+const api = require('./server/routes/api');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  'extended': 'false'
+}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/api', api);
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendStatus(204);
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
